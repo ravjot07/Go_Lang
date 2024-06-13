8c634d868ddf068c85c604e30b61cdbc21099943
ravjot07 <ravu2004@gmail.com>
2024-06-13 17:15:26 +0530

    csp example

---
+++ Goroutines,-Channels,-Communicating_Sequential_Processes/Parallel_get.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+func get(url string, ch chan<- result) {
+	start := time.Now()
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0} // error response
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t} // normal response
+		resp.Body.Close()
+	}
+
+}
+
+func main() {
+	results := make(chan result)
+	list := []string{"hello", "world"}
+
+	for _, url := range list {
+		go get(url, results) // start a CSP process
+	}
+	for range list { // read from the channel
+		r := <-results
+		if r.err != nil {
+			log.Printf("%-20s %s\n", r.url, r.latency)
+		} else {
+			log.Printf("%-20s %s\n", r.url, r.err)
+		}
+	}
+}
